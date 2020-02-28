21a3e5219b4f2f6380dad624528cc2dc06875581
jordnkr <jordnkr408@gmail.com>
2020-02-28 13:59:14 -0600

    updating tests and project variables

--- cypress/integration/HomeCostCalcAccessibility.spec.js
+++ cypress/integration/HomeCostCalcAccessibility.spec.js
@@ -1,22 +1,17 @@
-const A11Y_OPTIONS = {
-    runOnly: {
-        type: 'tag',
-        values: ['wcag2a', 'wcag2aa']
-    }
-}
-
-describe('Cash to close Accessibility', function() {
-    it('Passes accessibility tests', function() {
-        cy.visit('https://jordnkr.github.io/homeCostCalculator/cashToCloseCalculator.html')
-        cy.injectAxe()
-        cy.checkA11y()
+context('Home Cost Calculator axe-core testing', () => {
+    describe('Home page Accessibility', () => {
+        it('Passes accessibility tests', () => {
+            cy.visit('/')
+            cy.injectAxe()
+            cy.checkA11y()
+        })
     })
-})
-
-describe('Expected payments Accessibility', function() {
-    it('Passes accessibility tests', function() {
-        cy.visit('https://jordnkr.github.io/homeCostCalculator/expectedPaymentsCalculator.html')
-        cy.injectAxe()
-        cy.checkA11y()
+    
+    Object.keys(Cypress.env('endpoints')).forEach((url) => {
+        it ('Run axe-core against: ' + url, () => {
+            cy.visit(Cypress.env('endpoints')[url])
+            cy.injectAxe()
+            cy.checkA11y(Cypress.env('A11Y_OPTIONS'))
+        })
     })
 })
